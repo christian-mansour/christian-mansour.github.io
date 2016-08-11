13ef27af1a255e9072100a30d323e1d0097a5234
Christian Mansour <c.mansour@miami.edu>
2016-08-11 12:00:14 -0500

    initialize prototype commit

---
+++ script.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  $(window).load(function () {
+     setTimeout(function(){
+         $('#preloader').fadeOut('slow', function () {
+         });
+     },1000);
+   });
+});
